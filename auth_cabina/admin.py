f0e9ff76ee6115fd90deee9545da29c11d84889b
Israel <israel.paez14@outlook.com>
2020-08-04 12:40:18 -0600

    Mejora en el manejo de usuarios

--- auth_cabina/admin.py
+++ auth_cabina/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from django.contrib.admin import SimpleListFilter, ModelAdmin
+from django.contrib.admin import SimpleListFilter
 from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth.models import User
 
