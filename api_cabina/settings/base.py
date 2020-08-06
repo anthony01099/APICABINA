151465115b1c1a79adbad68bb9acb8ee40038b3e
Israel Gabriel Paez Montano <40218650+israelpaez14@users.noreply.github.com>
2020-08-06 10:36:43 -0600

    Update base.py

--- api_cabina/settings/base.py
+++ api_cabina/settings/base.py
@@ -77,13 +77,24 @@ WSGI_APPLICATION = 'api_cabina.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR,'..','db.sqlite3'),
+if DEBUG:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
     }
-}
+else:
+    DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.getenv('DATABASE_NAME'),
+        'USER': os.getenv('DATABASE_USERNAME'),
+        'PASSWORD': os.getenv('DATABASE_PASSWORD'),
+        'HOST': os.getenv('DATABASE_HOST'),
+        'PORT': '',
+        }
+    }
 
 
 # Password validation
