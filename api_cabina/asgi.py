ac1241ce98abf1de792567436b725426d44ccb0d
Israel Gabriel Paez Montano <40218650+israelpaez14@users.noreply.github.com>
2020-08-13 15:43:51 -0600

    Update asgi.py
    Cambios para WS

--- api_cabina/asgi.py
+++ api_cabina/asgi.py
@@ -8,9 +8,12 @@ https://docs.djangoproject.com/en/3.0/howto/deployment/asgi/
 """
 
 import os
+import django
+from channels.routing import get_default_application
 
 from django.core.asgi import get_asgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'api_cabina.settings')
 
-application = get_asgi_application()
+django.setup()
+
