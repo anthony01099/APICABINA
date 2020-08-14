d4a3b5874beb7ef26b4f3948f547b396a445d40e
jesuscol96 <jesus.colmenares435@gmail.com>
2020-08-14 14:11:17 -0400

    Add logs for django

--- api_cabina/settings/base.py
+++ api_cabina/settings/base.py
@@ -155,3 +155,20 @@ STATIC_URL = '/static/'
 #Media management
 MEDIA_ROOT = os.path.join(BASE_DIR,'..','media')
 MEDIA_URL = '/media/'
+
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'logfile': {
+            'class': 'logging.FileHandler',
+            'filename': '/var/log/django.log',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['logfile'],
+        },
+    },
+}
